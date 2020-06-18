da2e338e99e3200ad1301c5090ed6b33c87d34b2
Ieahleen <nethleen@gmail.com>
2020-06-18 15:16:49 +0000

    first feature: can randomize mount

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
 'use strict';
 
-const fs = require('fs');
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-let UUID = null;
+let UUID = null; 
 let apiKey = null;
 
-document.getElementById('submit-api-key').onclick = getUserDataJSON;
-
-function getUserDataJSON (){
-    console.log("getUserData")
-    UUID = document.getElementById("UUID").value;
+async function getUserDataJSON (){
+    UUID = document.getElementById("UUID").value; 
     apiKey = document.getElementById("api-key").value;
+    
+    const response = await fetch('https://habitica.com/api/v3/user?userFields=items', {
+        method: 'GET',
+        headers: {"x-api-user": UUID, "x-api-key": apiKey} 
+    }).then(r => r.json())
+    return response;
+}; 
 
-    const req = new XMLHttpRequest();
-    req.open("GET",'https://habitica.com/api/v3/user?userFields=items',true);
-    req.setRequestHeader('x-api-user', UUID);
-    req.setRequestHeader('x-api-key', apiKey);
-    req.send();
-    req.onload = buttonsFromUserData(req.responseText);
-};
+async function equipItem (type, key) {
+    const response = await fetch("https://habitica.com/api/v3/user/equip/" + type + "/" + key, {
+        method: "POST", 
+        headers: {"x-api-user": UUID, "x-api-key": apiKey}
+    }).then(res => res.json())
+    document.getElementById("header").append(response.success ? "Equipped a random thing: " + type + ": " + key : "Something went wrong!");
+}
 
-function buttonsFromUserData (userDataJSON) {
-    let userData = JSON.parse(userDataJSON);
-    let buttonsHTML = '';
-    let mounts = Object.keys(userData.data.items.mounts).filter(m => userData.data.items.mounts[m]);
+async function createRandomizeButtons () {
+    const data = await getUserDataJSON();
+
+    let html = ''; 
+
+    let mounts = Object.keys(data.data.items.mounts).filter(m => data.data.items.mounts[m]);
+    
     if (mounts.length > 0) {
-        var randomMount = mounts[Math.floor(Math.random() * mounts.length)];
-        buttonsHTML += '<input type="button" id="randomMount" text="Equip a random mount!">'
-        document.getElementById("randomMount").addEventListener("click", equipItem(randomMount))
+      html += '<input type="button" id="randomMount" value="Equip random mount">'
+    }
+
+    document.getElementById("main").innerHTML = html;
+
+    if (mounts.length > 0) {
+        document.getElementById("randomMount").addEventListener("click", () => {
+            let randomMount = mounts[Math.floor(mounts.length * Math.random())];
+            equipItem("mount", randomMount)
+        })
     }
 }
 
-function equipItem(type, key) {
-    const req = new XMLHttpRequest();
-    req.open("POST",'https://habitica.com/api/v3/user/equip/' + type + '/' + key,true);
-    req.setRequestHeader('x-api-user', UUID);
-    req.setRequestHeader('x-api-key', apiKey);
-    req.send();
-    // req.onload
-}
+document.getElementById('submit-api-key').addEventListener("click", createRandomizeButtons);
+
