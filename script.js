6b3c65ce30d194359f8994739aa1d3fcafc95782
Ieahleen <nethleen@gmail.com>
2020-07-08 07:42:10 +0000

    feature: start random quest

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function randomAnimals(mountsObj, petsObj, UUID, apiKey) {
     let html = '';
     let mounts = Object.keys(mountsObj).filter(m => mountsObj[m]);
     let pets = Object.keys(petsObj).filter(p => petsObj[p]);
-    html += '<p>Don\'t want to choose your next mount or pet? You can randomize with a click.</p><p>Each pet and mount has the same chance to come out, so it can happen that if currect equipped animal is selected, then it is just unequipped.</p>'
+    html += '<h2>Random Pet and Random Mount!</h2><p>Don\'t want to choose your next mount or pet? You can randomize with a click.</p><p>Each pet and mount has the same chance to come out, so it can happen that if currect equipped animal is selected, then it is just unequipped.</p>'
     // random mount button
     if (mounts.length > 0) {
       html += '<input type="button" id="randomMount" value="Equip random mount">'
@@ -80,7 +80,7 @@ function randomAnimals(mountsObj, petsObj, UUID, apiKey) {
 }
 
 function randomTransformationItem(specialObj, partyMembersArr, UUID, apiKey) {
-    let html = '<p>Do you have many party members and many transformation items and choosing is so much effort? No issue, just press a button, and no choice is necessary.</p>';
+    let html = '<h2>Random Transformation Item</h2><p>Do you have many party members and many transformation items and choosing is so much effort? No issue, just press a button, and no choice is necessary.</p>';
     let transformationItems = ["snowball", "spookySparkles", "seafoam", "shinySeed"].filter(i => specialObj[i] && specialObj[i] > 0);
     if (transformationItems.length > 0) {
         html += '<input type="button" id="randomTransformationItem" value="Cast random transformation item on random party member">'
@@ -106,7 +106,7 @@ function randomTransformationItem(specialObj, partyMembersArr, UUID, apiKey) {
 }
 
 function buyRandomEquipment (gp, c, eqArr, UUID, apiKey) {
-    let html = '<p>Do you have too much stuff to buy, after maybe emptying your inventory ';
+    let html = '<h2>Buy Random Equipment from the Market!</h2><p>Do you have too much stuff to buy, after maybe emptying your inventory ';
     html += 'by resetting your account, or kind request to an admin?';
     html += 'Just buy a random one using the button!</p>';
     eqArr = eqArr.filter(i => i.value <= gp && (i.klass === "special" || i.klass === c));
@@ -136,8 +136,8 @@ function buyRandomEquipment (gp, c, eqArr, UUID, apiKey) {
 }
 
 async function randomBackground (backgrounds, UUID, apiKey) {
-    let html = '<p>Don\'t know what to wear? Let the Random Numger Generator choose your background!</p>'
-    html += '<input type="button" id="equipRandomBackground" value="Equip random background">'
+    let html = '<h2>Equip a Random Background</h2><p>Don\'t know what to wear? Let the Random Numger Generator choose your background!</p>'
+    html += '<input type="button" id="equipRandomBackgroundButton" value="Equip random background">'
     html += '<p id="backgroundResponse"></p>'
 
     let div = document.createElement("div");
@@ -146,7 +146,7 @@ async function randomBackground (backgrounds, UUID, apiKey) {
     div.setAttribute("id", "equipRandomBackground");
     document.getElementById("main").appendChild(div);
 
-    document.getElementById("equipRandomBackground").addEventListener("click", async () => {
+    document.getElementById("equipRandomBackgroundButton").addEventListener("click", async () => {
         let backgroundToEquip = randomElememtFromArray(backgrounds);
         const response = await fetch("https://habitica.com/api/v3/user/unlock?path=background." + backgroundToEquip, {method: "POST", headers: {"x-api-user": UUID, "x-api-key": apiKey}})
                                             .then(resp => resp.json());
@@ -154,43 +154,119 @@ async function randomBackground (backgrounds, UUID, apiKey) {
     })
 }
 
+function startRandomQuest (questsObj, userLevel, headers) {
+    let html = '<h2>Start a random quest!</h2><p>Your party is not on a quest. Why don\'t you start one randomly right here? No need to choose it yourself!</p>';
+    
+    let questsArr = Object.keys(questsObj).filter(quest => {
+        if (questsObj[quest] && questsObj[quest] > 0) {
+            switch(quest) {
+                case 'atom1':
+                case 'atom2':
+                case 'atom3':
+                    return !userLevel < 15;
+                case 'vice1':
+                case 'vice2':
+                case 'vice3':
+                    return !userLevel < 30;
+                case 'goldenknight1':
+                case 'goldenknight2':
+                case 'goldenknight3':
+                    return !userLevel < 40;
+                case 'moonstone1':
+                case 'moonstone2':
+                case 'moonstone3':
+                    return !userLevel < 60;
+                default:
+                    return true;
+            }
+        }
+    
+    })
+    
+    if (questsArr.length > 0) {
+        html += '<input type="button" id="randomQuestButton" value="Start a random quest from your inventory">';
+        html += '<p id="randomQuestResponse"></p>';
+    } else {
+        html += '<p class="not-found">No quest to start found in your inventory.</p>'; 
+    }
+
+
+    let div = document.createElement("div");
+    div.innerHTML = html;
+    div.classList.add("wrapper");
+    div.setAttribute("id", "startRandomQuest");
+    document.getElementById("main").appendChild(div);
+
+    if (questsArr.length > 0) {
+        document.getElementById('randomQuestButton').addEventListener('click', async () => {
+            let randomQuest = randomElememtFromArray(questsArr);
+            await fetch("https://habitica.com/api/v3/groups/party/quests/invite/" + randomQuest, {method: "post", headers}).then(resp => resp.json);
+            document.getElementById("randomQuestResponse").innerText = "Invited party to quest: " + randomQuest;
+            document.getElementById("randomQuestButton").style.display = "none";
+        })
+    }
+    
+    
+
+}
+
 document.getElementById('submit-api-key').addEventListener("click", async () => {
     let UUID = document.getElementById("UUID").value; 
     let apiKey = document.getElementById("api-key").value;  
+    let headers = {
+                            "x-api-user": UUID,
+                            "x-api-key": apiKey
+                        }
     
     const {
         data : {
             items : {
                 mounts : mountsObj,
                 pets: petsObj, 
-                special: specialObj
+                special: specialObj,
+                quests: questsObj
             },
             stats : {
                 gp : goldOwned,
-                class : userClass
+                class : userClass,
+                lvl : userLevel
             },
             purchased : {
                 background : backgroundsObj
             }
         }
-    } = await fetch('https://habitica.com/api/v3/user', {method: 'GET', headers: {"x-api-user": UUID, "x-api-key": apiKey}}).then(r => r.json());
+    } = await fetch('https://habitica.com/api/v3/user', {method: 'GET', headers}).then(r => r.json());
     
     const {
         success : partyDataWasFound,
         error,
         data : partyMembersArr
-    } = await fetch('https://habitica.com/api/v3/groups/party/members', {method: 'GET', headers: {"x-api-user": UUID, "x-api-key": apiKey}}).then(r => r.json());
+    } = await fetch('https://habitica.com/api/v3/groups/party/members', 
+                    {
+                        method: 'GET', headers                      
+                        
+                    }).then(r => r.json());
     
     const {
         data : availableEquipmentArr
-    } = await fetch('https://habitica.com/api/v3/user/inventory/buy', {method: 'GET', headers: {"x-api-user": UUID, "x-api-key": apiKey}}).then(r => r.json());
+    } = await fetch('https://habitica.com/api/v3/user/inventory/buy', {method: 'GET', headers}).then(r => r.json());
     
+    const {
+        data : {
+            quest
+        }
+    } = await fetch('https://habitica.com/api/v3/groups/party', {method: 'GET', headers})
+                        .then(r => r.json())
     document.getElementById("main").innerHTML = "";
 
     randomAnimals(mountsObj, petsObj, UUID, apiKey);
 
     if (partyDataWasFound) {
         randomTransformationItem(specialObj, partyMembersArr, UUID, apiKey);
+        console.log(quest.key);
+        if (!quest.key) {
+            startRandomQuest(questsObj, userLevel, headers);
+        }
     }
 
     randomBackground(Object.keys(backgroundsObj), UUID, apiKey)
